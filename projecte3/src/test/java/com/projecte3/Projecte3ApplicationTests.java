84c68619fd0a7543c032aedcc7d6f92e6b763809
Nuria Sy <2526_nuria.sy@iticbcn.cat>
2026-05-18 17:15:39 +0200

    Afegit proojecte

---
+++ projecte3/src/test/java/com/projecte3/Projecte3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projecte3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projecte3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
